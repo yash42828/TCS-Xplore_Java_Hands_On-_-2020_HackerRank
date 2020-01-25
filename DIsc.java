da314bb190abd79f0315617f44c184fbc11ae88d
yash42828 <bajajyash42828@gmail.com>
2020-01-25 11:08:24 +0530

    sort

--- DIsc.java
+++ DIsc.java
@@ -1,9 +1,10 @@
 import java.util.*;
 
-public class Disc {
+public class Solu {
     public static void main(String args[] ) throws Exception {
         /* Do not alter code in main method */
-       Shirt[] shirts = new Shirt[5];
+       //Shirt[] shirts = new Shirt[5];
+       List<Shirt> shirts = new ArrayList<Shirt>();
         
         Scanner sc = new Scanner(System.in);
         
@@ -13,7 +14,7 @@ public class Disc {
             String brand = sc.nextLine();
             double price = sc.nextDouble();sc.nextLine();
             char g = sc.nextLine().charAt(0);
-            shirts[i] = new Shirt(tag,brand,price,g);
+            shirts.add(new Shirt(tag,brand,price,g));
         }
         
         
@@ -25,73 +26,76 @@ public class Disc {
             System.out.println(getDiscountPrice(s));            
         }
         
-        Shirt[] result = getShirtWithMoreThanSpecificPrice(shirts,price);
-        //System.out.println(getShirtWithMoreThanSpecificPrice(shirts,price));
+        List<Shirt> result = getShirtWithMoreThanSpecificPrice(shirts,price);
+        
         for(Shirt s: result)
         {
-			if(s.brand != null){
             System.out.println(s.getTag()+" "+s.getPrice()+ " " + s.getBrand());
-			}
         }
     }
-	
     
-    /* implement your methods here*/
-	public static double getDiscountPrice(Shirt s){
-					double temp = s.price;
+    public static double getDiscountPrice(Shirt s){
+                    double temp = s.price;
 
-		if(s.gender == 'm'){
-			temp = temp - (temp*10)/100;
-			//return s.price;
-		}
-		if(s.gender == 'f'){
-			//double temp = s.price;
-			temp = temp - (temp*20)/100;
-			//return s.price;
-		}
-		if(s.gender == 'u'){
-			//double temp = s.price;
-			temp = temp - (temp*30)/100;
-			//return s.price;
-		}
-		return temp;
-	}
-	public static Shirt[] getShirtWithMoreThanSpecificPrice(Shirt[] shirts, double price){
-		int l = 0;
-		for(Shirt sh:shirts){
-			if(sh.price>price){
-				l++;
-			}
-		}
-		Shirt[] sht = new Shirt[l];
-		//List<Shirt> sht = new ArrayList<Shirt>();
-		int k =0;
-		for(Shirt sh:shirts){
-			if(sh.price>price){
-				sht[k] = sh;
-				k++;
-			}
-		}
-		return sht;
-		
-	}
+        if(s.gender == 'm'){
+            temp = temp - (temp*10)/100;
+            //return s.price;
+        }
+        if(s.gender == 'f'){
+            //double temp = s.price;
+            temp = temp - (temp*20)/100;
+            //return s.price;
+        }
+        if(s.gender == 'u'){
+            //double temp = s.price;
+            temp = temp - (temp*30)/100;
+            //return s.price;
+        }
+        return temp;
+    }
+    public static List<Shirt> getShirtWithMoreThanSpecificPrice(List<Shirt> shirts, double price){
+        int l = 0;
+        for(Shirt sh:shirts){
+            if(sh.price>price){
+                l++;
+            }
+        }
+        //Shirt[] sht = new Shirt[l];
+        List<Shirt> sht = new ArrayList<Shirt>();
+        //int k =0;
+        for(Shirt sh:shirts){
+            if(sh.price>price){
+                //sht[k] = sh;
+                sht.add(sh);
+                //k++;
+            }
+        }
+		Collections.sort(sht);
+        return sht;
+        
+    }
+
 }
 
-class Shirt
+class Shirt implements Comparable<Shirt>
 {
     //define the class as per details shared in the question
-	int tag;
-	String brand;
-	double price;
-	char gender;
-	Shirt(int tag, String brand, double price, char gender){
-		this.tag = tag;
-		this.brand = brand;
-		this.price = price;
-		this.gender = gender;
-	}
-	public int getTag(){return this.tag;}
-	public String getBrand(){return this.brand;}
-	public double getPrice(){return this.price;}
-	
+    int tag;
+    String brand;
+    double price;
+    char gender;
+    Shirt(int tag, String brand, double price, char gender){
+        this.tag = tag;
+        this.brand = brand;
+        this.price = price;
+        this.gender = gender;
+    }
+    public int getTag(){return this.tag;}
+    public String getBrand(){return this.brand;}
+    public double getPrice(){return this.price;}
+    public Double getPrices(){return price;}
+    @Override
+    public int compareTo(Shirt s){
+        return this.getPrices().compareTo(s.getPrices());
+    }
 }
--- Title.java
+++ Title.java
@@ -1,7 +1,8 @@
 import java.util.*;
 class Title{
 	public static void main(String args[]) {
-	Book[] bk = new Book[4];
+	//Book[] bk = new Book[4];
+	List<Book> bk = new ArrayList<Book>();
 	Scanner sc = new Scanner(System.in);
 	for(int i = 0;i<4;i++)
     {
@@ -9,17 +10,19 @@ class Title{
         String title = sc.nextLine();
         String author = sc.nextLine();
         double price = sc.nextDouble();sc.nextLine();
-        bk[i] = new Book(id,title,author,price);
+        bk.add(new Book(id,title,author,price));
     }
     String search = sc.nextLine();
 
-    Book[] res = searchTitle(bk,search);
+//    Book[] res = searchTitle(bk,search);
+	List<Book> res = searchTitle(bk,search);
+
     for(Book bk1:res) {
     	System.out.println(bk1.id);
     }
 }    
 
-	public static Book[] searchTitle(Book[] b, String s){
+	public static List<Book> searchTitle(List<Book> b, String s){
 		int l = 0;
 		int k = 0;
 		for(Book b1:b) {
@@ -27,18 +30,22 @@ class Title{
 				l++;
 			}
 		}
-		Book[] result = new Book[l];
+		//Book[] result = new Book[l];
+			List<Book> result = new ArrayList<Book>();
+
 		for(Book b1:b) {
 			if(b1.title.toLowerCase().contains(s.toLowerCase())) {
-				result[k] = b1;
+				//result[k] = b1;
+				result.add(b1);
 				k++;
 			}
 		}
+		Collections.sort(result);
 		return result;
 	}
 }
 
-class Book{
+class Book implements Comparable<Book>{
 	int id;
 	String title;
 	String author;
@@ -49,5 +56,10 @@ class Book{
 		this.title = title;
 		this.author = author;
 		this.price = price;
+	}
+	public Integer getPrices(){return id;}
+    @Override
+    public int compareTo(Book s){
+        return this.getPrices().compareTo(s.getPrices());
 	}
 }
